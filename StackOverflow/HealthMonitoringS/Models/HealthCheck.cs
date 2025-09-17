93374a378156bd811c7626cfc242d851bc2d31e8
Iva <iiva6070@gmail.com>
2025-09-17 10:05:19 +0200

    osnovna implementacija healthmonitoring-a

---
+++ StackOverflow/HealthMonitoringS/Models/HealthCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Collections.Concurrent;
+
+namespace HealthMonitoringS.Models
+{
+    public class HealthCheck : TableEntity
+    {
+        public HealthCheck() { }
+
+        public HealthCheck(string serviceName)
+        {
+            PartitionKey = serviceName;        // naziv servisa
+            RowKey = Guid.NewGuid().ToString(); // jedinstveni ID
+        }
+
+        public string Status { get; set; } // "OK" ili "NOT_OK"
+
+
+        public DateTime CheckedAt { get; set; } = DateTime.UtcNow;
+    }
+}
--- StackOverflow/HealthMonitoringS/WorkerRole.cs
+++ StackOverflow/HealthMonitoringS/WorkerRole.cs
@@ -1,13 +1,16 @@
 using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Diagnostics;
 using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.WindowsAzure.Storage.Queue; // dodaj za queue
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using HealthMonitoringS.Models;
+using System.Net;
 
 namespace HealthMonitoringS
 {
@@ -15,11 +18,31 @@ namespace HealthMonitoringS
     {
         private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
         private readonly ManualResetEvent runCompleteEvent = new ManualResetEvent(false);
+        private HttpClient httpClient;
+        private CloudQueue alertsQueue; // queue za NOT_OK dogaðaje
+
+        public override bool OnStart()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            httpClient = new HttpClient();
+
+            // Inicijalizacija alerts queue
+            var connectionString = RoleEnvironment.GetConfigurationSettingValue("StorageConnectionString");
+            var storageAccount = CloudStorageAccount.Parse(connectionString);
+            var queueClient = storageAccount.CreateCloudQueueClient();
+            alertsQueue = queueClient.GetQueueReference("alerts");
+            alertsQueue.CreateIfNotExists();
+
+            bool result = base.OnStart();
+            Trace.TraceInformation("HealthMonitoringS has been started");
+            return result;
+        }
 
         public override void Run()
         {
             Trace.TraceInformation("HealthMonitoringS is running");
-
             try
             {
                 this.RunAsync(this.cancellationTokenSource.Token).Wait();
@@ -30,43 +53,61 @@ namespace HealthMonitoringS
             }
         }
 
-        public override bool OnStart()
-        {
-            // Use TLS 1.2 for Service Bus connections
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-
-            // Set the maximum number of concurrent connections
-            ServicePointManager.DefaultConnectionLimit = 12;
-
-            // For information on handling configuration changes
-            // see the MSDN topic at https://go.microsoft.com/fwlink/?LinkId=166357.
-
-            bool result = base.OnStart();
-
-            Trace.TraceInformation("HealthMonitoringS has been started");
-
-            return result;
-        }
-
         public override void OnStop()
         {
             Trace.TraceInformation("HealthMonitoringS is stopping");
-
             this.cancellationTokenSource.Cancel();
             this.runCompleteEvent.WaitOne();
-
             base.OnStop();
-
             Trace.TraceInformation("HealthMonitoringS has stopped");
         }
 
         private async Task RunAsync(CancellationToken cancellationToken)
         {
-            // TODO: Replace the following with your own logic.
+            // Azure Table
+            string connectionString = RoleEnvironment.GetConfigurationSettingValue("StorageConnectionString");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable healthTable = tableClient.GetTableReference("HealthCheck");
+            healthTable.CreateIfNotExists();
+
             while (!cancellationToken.IsCancellationRequested)
             {
-                Trace.TraceInformation("Working");
-                await Task.Delay(1000);
+                await CheckServiceAsync("StackOverflowService", "http://localhost:5059/health-monitoring", healthTable);
+                await CheckServiceAsync("NotificationService", "http://localhost:5060/health-monitoring", healthTable);
+
+                await Task.Delay(4000); // 4 sekunde
+            }
+        }
+
+        private async Task CheckServiceAsync(string serviceName, string url, CloudTable table)
+        {
+            // kreiramo entitet i prosleðujemo serviceName u konstruktor
+            HealthCheck entity = new HealthCheck(serviceName)
+            {
+                CheckedAt = DateTime.UtcNow
+            };
+
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                entity.Status = response.IsSuccessStatusCode ? "OK" : "NOT_OK";
+            }
+            catch
+            {
+                entity.Status = "NOT_OK";
+            }
+
+            // Upis u tabelu
+            TableOperation insertOperation = TableOperation.Insert(entity);
+            await table.ExecuteAsync(insertOperation);
+
+            // Ako je NOT_OK, šaljemo poruku u alerts queue
+            if (entity.Status == "NOT_OK")
+            {
+                string alertMessage = $"{serviceName} nije dostupan u {DateTime.UtcNow}";
+                CloudQueueMessage message = new CloudQueueMessage(alertMessage);
+                await alertsQueue.AddMessageAsync(message);
             }
         }
     }
